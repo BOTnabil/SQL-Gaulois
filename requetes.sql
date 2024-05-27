8805270a2d46016f5c958b0f7f01475d10b88d76
Nabil <l4teoyt@gmail.com>
2024-05-27 11:10:47 +0200

    Question 2

--- requetes.sql
+++ requetes.sql
@@ -4,3 +4,11 @@ SELECT *
 FROM lieu
 WHERE nom_lieu
 LIKE '%um'
+
+-- 2. Nombre de personnages par lieu (trié par nombre de personnages décroissant).
+
+SELECT COUNT(nom_personnage) AS nbrePersonnages, lieu.nom_lieu
+FROM personnage
+INNER JOIN lieu ON personnage.id_lieu = lieu.id_lieu
+GROUP BY lieu.id_lieu
+ORDER BY nbrePersonnages DESC
