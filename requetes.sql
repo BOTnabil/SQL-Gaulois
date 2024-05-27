97d23a82a9ee11e73bb996e5641586d1ad33d082
Nabil <l4teoyt@gmail.com>
2024-05-27 11:36:36 +0200

    Question 6

--- requetes.sql
+++ requetes.sql
@@ -36,3 +36,12 @@ SELECT nom_bataille, DATE_FORMAT(date_bataille, "%d %m %Y") AS dateBataille, lie
 FROM bataille
 INNER JOIN lieu ON bataille.id_lieu = lieu.id_lieu
 ORDER BY date_bataille DESC
+
+-- 6. Nom des potions + coût de réalisation de la potion (trié par coût décroissant).
+
+SELECT potion.nom_potion, SUM(qte * ingredient.cout_ingredient) AS coutDeReal
+FROM  composer
+INNER JOIN ingredient ON ingredient.id_ingredient = composer.id_ingredient
+INNER JOIN potion ON composer.id_potion = potion.id_potion
+GROUP BY potion.nom_potion
+ORDER BY coutDeReal DESC
