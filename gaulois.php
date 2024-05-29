4b4362415aaae067dc0bab67b318ac57207ce826
Nabil <165811166+BOTnabil@users.noreply.github.com>
2024-05-29 15:26:48 +0200

    Update gaulois.php

--- gaulois.php
+++ gaulois.php
@@ -1,4 +1,5 @@
 <?php
+
 try
 {
 	$mysqlClient = new PDO('mysql:host=localhost;dbname=gaulois;charset=utf8', 'root', '');
@@ -8,10 +9,11 @@ catch (Exception $e)
     die('Erreur : ' . $e->getMessage());
 }
 
-$sqlQuery = 'SELECT nom_personnage, lieu.nom_lieu, specialite.nom_specialite
+$sqlQuery = 'SELECT nom_personnage, lieu.nom_lieu, specialite.nom_specialite, id_personnage
 FROM personnage
 JOIN lieu ON personnage.id_lieu = lieu.id_lieu
-JOIN specialite ON personnage.id_specialite = specialite.id_specialite';
+JOIN specialite ON personnage.id_specialite = specialite.id_specialite
+ORDER BY lieu.nom_lieu, specialite.nom_specialite';
 $persoStatement = $mysqlClient->prepare($sqlQuery);
 $persoStatement -> execute();
 $personnages = $persoStatement->fetchAll();
@@ -19,15 +21,15 @@ $personnages = $persoStatement->fetchAll();
 echo "<table>
         <tr>
             <th>Nom</th>
-			<th>Specialité<th>
             <th>Lieu d'habitation</th>
+			<th>Specialité<th>
         </tr>";
 
 foreach ($personnages as $personnage) {
     echo "<tr>
-    		<td>".$personnage['nom_personnage']."</td>
-			<td>".$personnage['nom_specialite']."</td>
+    		<td><a href='personnage.php?id=".$personnage['id_personnage']."'>".$personnage['nom_personnage']."</a></td>
     		<td>".$personnage['nom_lieu']."</td>
+			<td>".$personnage['nom_specialite']."</td>
 		</tr>";
 }
 
